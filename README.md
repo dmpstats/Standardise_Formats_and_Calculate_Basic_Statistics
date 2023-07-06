cabc4566f16b3a5dc83028032522239e1a1d33d1
cjc31 <cjc31@st-andrews.ac.uk>
2023-07-06 09:24:12 +0100

    Committing first draft for MoveApps upload

--- README.md
+++ README.md
@@ -1,39 +1,89 @@
-# Name of App *(Give your app a short and informative title. Please adhere to our convention of Title Case without hyphens (e.g. My New App))*
+---
+editor_options: 
+  markdown: 
+    wrap: sentence
+---
+
+# Data Processing for Merging Studies
 
 MoveApps
 
-Github repository: *github.com/yourAccount/Name-of-App* *(the link to the repository where the code of the app can be found must be provided)*
+Github repository: [*https://github.com/callumjclarke/preprocessing*](https://github.com/callumjclarke/preprocessing){.uri}
 
 ## Description
-*Enter here the short description of the App that might also be used when filling out the description when submitting the App to Moveapps. This text is directly presented to Users that look through the list of Apps when compiling Workflows.*
+
+This MoveApp contains settings to perform various basic cleaning and binding processes, ranging from binding and renaming columns using standardized names to filtering the data to intervals of predefined duration.
+Any combination of the processes detailed below can be selected.
 
 ## Documentation
-*Enter here a detailed description of your App. What is it intended to be used for. Which steps of analyses are performed and how. Please be explicit about any detail that is important for use and understanding of the App and its outcomes.*
+
+This MoveApp fulfills several purposes in a workflow.
+Its overall aim is to standardize any input study into a form that can be merged at a later stage in a workflow with additional studies (that have also been processed) to avoid discrepancies in study names, units, data volume, and/or column names.
+
+It contains options to: \* Filter the data to intervals of X minutes \* Append columns containing Universal Transverse Mercator (UTM) data \* Append an *index* column, consisting of the animal's ID and timestamp concatenated \* Append columns containing *speed*, *distance traveled* and/or *time since previous point* data \* Generate columns containing specific timestamp data (hour, minute and second, independently) \* Standardize column names for *altitude*, *temperature* and *heading* data across studies
 
 ### Input data
-*Indicate which type of input data the App requires. Currently only R objects of class `MoveStack` can be used. This will be extend in the future.*
 
-*Example*: MoveStack in Movebank format
+Move2 location object
 
 ### Output data
-*Indicate which type of output data the App produces to be passed on to subsequent apps. Currently only R objects of class `MoveStack` can be used. This will be extend in the future. In case the App does not pass on any data (e.g. a shiny visualization app), it can be also indicated here that no output is produced to be used in subsequent apps.*
 
-*Example:* MoveStack in Movebank format
+Move2 location object
 
 ### Artefacts
-*If the App creates artefacts (e.g. csv, pdf, jpeg, shapefiles, etc), please list them here and describe each.*
 
-*Example:* `rest_overview.csv`: csv-file with Table of all rest site properties
+None.
 
-### Settings 
-*Please list and define all settings/parameters that the App requires to be set by the App user, if necessary including their unit.*
+### Settings
 
-*Example:* `Radius of resting site` (radius): Defined radius the animal has to stay in for a given duration of time for it to be considered resting site. Unit: `metres`.
+`Filter by Time Interval` (integer): The length of interval, in minutes, to which to filter data.
+NULL means no filtering will take place.
+Defaults to 5-minute intervals.
+`move2::mt_filter_per_interval(criterion = first)` is used for this filtering.
+
+`Bind additional timestamp columns` (logical): Determines whether to append specific timestamp data (*hour,* *minute,* *second,* *hourmin,* and *yearmonthday*) to the output.
+If FALSE, overrides *Keep Essential Columns*.
+
+`Bind UTM location data` (logical): Determines whether to generate and append Universal Transverse Mercator (UTM) data to the output.
+If TRUE, the primary geometry will become UTM data.
+NOTE: This will also remove points without any attached geometry.
+
+`EPSG` (integer): If *Bind UTM location data* is selected, please provide a valid EPSG code for the transformed coordinate system.
+Defaults to EPSG:32733 (UTM zone 33S).
+
+`Bind index` (logical): Determines whether to append an event 'index' to the data, consisting of the tag's ID concatenated with its timestamp.
+If FALSE, overrides *Keep Essential Columns*.
+`Bind km/h` (logical): Determines whether to append a speed column (units: km/h).
+
+`Bind distance` (logical): Determines whether to append a *distance travelled* from previous event column (units: metres).
+
+`Bind time difference` (logical): Determines whether to append a *time difference* from previous event column (units: hours).
+
+`Bind study` (logical): determines whether to append a *study name* column for each animal.
+This is recommended if merging with other studies later in this workflow
+
+`ID Column` (character): The name of the column to be used as primary identification.
+NULL means the default (taken from Movebank) will be used.
+
+`Altitude Column` (character): The name of the column containing altitude data (if any).
+NULL means no altitude data is available.
+
+`Temperature Column` (character): The name of the column containing temperature data (if any).
+NULL means no temperature data is available.
+
+`Heading Column` (character): The name of the column containing heading data (if any).
+NULL means no heading data is available.
+
+`Keep Essential Columns` (logical): If TRUE, the output data contains only the following columns present: *temperature*, *heading*, *altitude*, *import_marked_outlier*, *index*, *hour*, *min*, *secs*, *hourmin*, *yearmonthday*, *gap_mins*, *kmph*, *dist_m*, *x (UTM)*, *y (UTM)*, *geometry (sf)*, *lon*, *lat*, *study*.
 
 ### Most common errors
-*Please describe shortly what most common errors of the App can be, how they occur and best ways of solving them.*
+
+-   `bind study` setting currently works only if all tracks in the data come from the same study, and if `study.id` or `study_id` is a column name in the track data. It will be unable to perform the operation if not
+-   Any `column` settings can be bugged by a column name with two periods: for example, 'altitude.col.xyz' could not be recognized. *Please describe shortly what most common errors of the App can be, how they occur and best ways of solving them.*
 
 ### Null or error handling
+
 *Please indicate for each setting as well as the input data which behaviour the App is supposed to show in case of errors or NULL values/input. Please also add notes of possible errors that can happen if settings/parameters are improperly set and any other important information that you find the user should be aware of.*
 
-*Example:* **Setting `radius`:** If no radius AND no duration are given, the input data set is returned with a warning. If no radius is given (NULL), but a duration is defined then a default radius of 1000m = 1km is set. 
+*Example:* **Setting `radius`:** If no radius AND no duration are given, the input data set is returned with a warning.
+If no radius is given (NULL), but a duration is defined then a default radius of 1000m = 1km is set.
--- RFunction.R
+++ RFunction.R
@@ -1,36 +1,267 @@
-library('lubridate')
+library('move2')
+library('dplyr')
+library('stringr')
+library('magrittr')
+require('lubridate')
 
-## The parameter "data" is reserved for the data object passed on from the previous app
+# Call useful function for later
+`%!in%` <- Negate(`%in%`)
 
-# to display messages to the user in the log file of the App in MoveApps
-# one can use the function from the logger.R file:
-# logger.fatal(), logger.error(), logger.warn(), logger.info(), logger.debug(), logger.trace()
+# MoveApp settings
+rFunction = function(data, timefilter = 5, 
+                     bind_times = TRUE, 
+                     createUTMs = TRUE,
+                     EPSG = 32733,
+                     bind_index = TRUE, 
+                     bind_kmph = TRUE,
+                     bind_dist = TRUE,
+                     bind_timediff = TRUE,
+                     bind_study = TRUE,
+                     idcol = NULL, 
+                     altitudecol = NULL, 
+                     tempcol = NULL, 
+                     headingcol = NULL, 
+                     keepessentials = TRUE) {
 
-# Showcase injecting app setting (parameter `year`)
-rFunction = function(data, sdk, year, ...) {
-  logger.info(paste("Welcome to the", sdk))
-  result <- if (any(lubridate::year(mt_time(data)) == year)) { 
-    data[lubridate::year(mt_time(data)) == year,]
-  } else {
-    NULL
+
+  # Generate optional columns --------------------------------------------------------
+
+  colheadings <- c(altitudecol, tempcol, idcol, headingcol)
+  colheadings <- Filter(Negate(is.null), colheadings)
+  
+  if(any(colheadings %!in% colnames(data))) {
+    missing <- colheadings[which(colheadings %!in% colnames(data))]
+    logger.fatal(paste0("One of the input column names is not present in this dataset. Please check input settings carefully. Missing column(s): ", toString(missing)))
+    return(data)
   }
-  if (!is.null(result)) {
-    # Showcase creating an app artifact. 
-    # This artifact can be downloaded by the workflow user on Moveapps.
-    artifact <- appArtifactPath("plot.png")
-    logger.info(paste("plotting to artifact:", artifact))
-    png(artifact)
-    plot(result)
-    dev.off()
-  } else {
-    logger.warn("nothing to plot")
-  }
-  # Showcase to access a file ('auxiliary files') that is 
-  # a) provided by the app-developer and 
-  # b) can be overridden by the workflow user.
-  fileName <- paste0(getAppFilePath("yourLocalFileSettingId"), "sample.txt")
-  logger.info(readChar(fileName, file.info(fileName)$size))
+  
 
-  # provide my result to the next app in the MoveApps workflow
+  # Process altitude data
+  if(is.null(altitudecol)) {
+    data %<>% mutate(altitude = NA)
+  } else {
+    # If altitude is present, rename the column
+    try(data %<>% rename(altitude = altitudecol))
+
+  }
+  
+  # Process temperature data
+  if(is.null(tempcol)) {
+    data %<>% mutate(temperature = NA)
+  } else {
+    # If altitude is present, rename the column
+    try(data %<>% rename(temperature = tempcol))
+  }
+  
+  # Process heading data
+  if(is.null(headingcol)) {
+    data %<>% mutate(heading = NA)
+  } else {
+    # If altitude is present, rename the column
+    try(data %<>% rename(heading = headingcol))
+  }
+  
+  # Define trackID 
+  if(!is.null(idcol)) {
+    logger.info(paste0("Changing primary ID column to ", idcol))
+    mt_track_data(data)
+    data <- mt_set_track_id_column(data, idcol)
+  }
+  
+  # Add study name
+  if(bind_study == TRUE) {
+    
+    if("study.id" %!in% colnames(mt_track_data(data)) & "study_id" %!in% colnames(mt_track_data(data))) {
+      logger.warn("study.id and study_id are not columns in the track data. Unable to bind study column")
+    } else {
+      
+      if("study.id" %in% colnames(mt_track_data(data))) {
+        study_id <- mt_track_data(data)$study.id
+      } else {
+        study_id <- mt_track_data(data)$study_id
+      }
+      
+      studyname <- movebank_download_study_info(id = study_id)$name
+      
+      # CC: I'll update this to work on >1 studies later
+      if(length(unique(study_id)) > 1) {
+        logger.warn("Input contains data from several studies. Study ID will not be appended.")
+      } else {
+        data %<>% mutate(study = rep(studyname, nrow(data)))
+        logger.info("Study column appended")
+      }
+      
+    }
+    
+  }
+  
+  # Add indexes -----------------------------------------------------------------
+
+  logger.info("Binding indexes for each tag")
+  data %<>% mutate(
+    index = paste0(mt_track_id(data), " ", mt_time(data))
+  )
+
+  
+  # Bind time columns ----------------------------------------------------------
+
+  
+  logger.info("Generating additional timestamp data")
+  data <- data %>%
+    dplyr::mutate(
+      hour = hour(mt_time(data)),
+      min = minute(mt_time(data)),
+      secs = second(mt_time(data)),
+      hourmin = hour + min/60 + secs/3600,
+      day = day(mt_time(data)),
+      month = month(mt_time(data)),
+      year = year(mt_time(data)),
+      yearmonthday = stringr::str_replace_all(str_sub(mt_time(data), 1, 10), "-", ""),
+      gap_mins = difftime(mt_time(data), lag(mt_time(data)), units = "mins"))  
+  
+  
+  # Filter to predefined intervals --------------------------------------------------
+  
+  # We should tr mt_filter_by_interval in place of this section
+  # Hopefully less computationally demanding
+
+  if(!is.null(timefilter)) {
+
+    logger.info(paste0("Filtering to bins of duration ", timefilter, " minutes"))
+    timeunit <- paste0(as.character(timefilter), " mins")
+    
+    data %<>% mt_filter_per_interval(criterion = "first", unit = timeunit)
+    
+    # # generate necessary cols (if don't already exist)
+    # if("gap_mins" %!in% colnames(data)) {
+    #   gap_mins = difftime(mt_time(data), lag(mt_time(data)), units = "mins")
+    # }
+    # 
+    # 
+    # 
+    # data <- data %>% 
+    #   mutate(temptag = mt_track_id(data)) %>% # used to sort below
+    #   dplyr::mutate(binmin = ceiling_date(mt_time(data), unit = minutes(timefilter))) # create time bins for filtering
+    # 
+    # logger.info("Bins created. Grouping and summarising")
+    # # The following is slow. We should try to find alternatives
+    # data <- data %>%
+    #   group_by(temptag, binmin) %>% 
+    #   summarise_all(.funs = first) # need original obj names
+    # 
+    # logger.info("Returning filtered data to move2 object")
+    # # Return object to move2 class, as summarise() changes to sf
+    # drop.cols <- c('binmin', 'temptag', 'gap_mins')
+    # data <- mt_as_move2(data, track_id_column = trackname, time_column = timename) %>%
+    #   select(-one_of(drop.cols))
+    
+
+    logger.info("Filtering completed.")
+  }
+  
+  if(!mt_is_move2(data)) {logger.fatal("Data is no longer move2 object after filtering - can't be passed onto next MoveApp")}
+  
+
+  # Append UTM data --------------------------------------------------------------
+  
+
+  if(createUTMs == TRUE) {
+    logger.info("Binding UTM data.")
+    
+    # Filter out points without attached geometry
+    latlons <- sf::st_coordinates(data)
+    data %<>% mutate(lon = latlons[, 1],
+                     lat = latlons[, 2])
+    remove <- which(is.na(latlons), arr.ind=TRUE)[, 1]
+    removeindex <- data$index[remove]
+    
+    # Check for points without associated geometry and report:
+    if(identical(remove, integer(0))) {
+      logger.info("No empty points located. Proceeding with UTMs.")
+    } else {
+      logger.warn(paste0("Empty point(s) located, with indexes: ", 
+                         ifelse(bind_index, removeindex, "[INDEXES UNAVAILABLE. PLEASE USE 'bind_index' PROPERTY IN SETTINGS]")
+      ))
+
+      logger.warn("Removing empty points and continuing with UTMs.")
+      data %<>% filter(index %!in% removeindex)
+
+
+    }
+    
+    # Make UTMs primary data geometry
+    logger.info("Changing primary coordinates to UTM data")
+    crscode <- sf::st_crs(EPSG) # retrieve CRS code for given EPSG
+    data %<>% sf::st_transform(crscode)
+    
+    newcoords <- sf::st_coordinates(data)
+    data %<>% mutate(x = newcoords[, 1],
+                     y = newcoords[, 2])
+    
+
+
+  }
+  
+  if(!mt_is_move2(data)) {logger.fatal("Data is no longer move2 object after UTM data appending - can't be passed onto next MoveApp")}
+
+  
+  # Append speeds and final data ------------------------------------------------------
+  
+  if(bind_kmph == TRUE) {
+    data$kmph <- as.vector(mt_speed(data) * 0.06) # convert to kmph
+  }
+  if(bind_dist == TRUE) {
+    data$dist_m <- as.vector(mt_distance(data))
+  }
+  if(bind_timediff == TRUE) {
+    data %<>% mutate(
+      timediff_hrs =   as.numeric(difftime(mt_time(data), lag(mt_time(data), default = mt_time(data)[1]), units = "hours"))
+    )
+  }
+  
+  
+  
+
+  # Select only essential columns --------------------------------------------------------
+  
+  essentialcols <- c(mt_track_id_column(data), 
+                     mt_time_column(data), 
+                     "temperature", 
+                     "heading", 
+                     "altitude", 
+                     "import_marked_outlier", 
+                     "index", 
+                     "hour", 
+                     "min",
+                     "secs",
+                     "hourmin",
+                     "yearmonthday",
+                     "gap_mins",
+                     "kmph",
+                     "dist_m",
+                     "x",
+                     "y",
+                     "geometry",
+                     "lon", 
+                     "lat",
+                     "study"
+  )
+  if(keepessentials == TRUE) {
+    data %<>% select(any_of(essentialcols))
+  }
+  
+  # Remove index if desired
+  if(bind_index == FALSE) {
+    data %<>% select(any_of(-index))
+  }
+  
+  # Remove times if desired
+  if(bind_times == FALSE) {
+    data %<>% select(any_of(-hour, -min, -secs, -hourmin, -yearmonthday, -gap_mins))
+  }
+  
+  # Return --------------------------------------------------------------------------
+  
+  result <- data
   return(result)
 }
