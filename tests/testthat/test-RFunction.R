7587516d744b9eb3fda13054cacd66ce112a0f67
Bruno Caneco <bruno@dmpstats.com>
2023-10-31 13:17:28 +0000

    unit tests added

--- tests/testthat/test-RFunction.R
+++ tests/testthat/test-RFunction.R
@@ -1,3 +1,5 @@
+library(units)
+
 test_data <- test_data("input3.rds")
 
 test_that("timefilter works", {
@@ -18,6 +20,34 @@ test_that("timefilter works", {
   expect_condition(min(mt_time_lags(actual)) > lubridate::minutes(5))
   
 })
+
+
+
+test_that("`speedcut` works", {
+  
+  speed_thresh <- 100
+  
+  actual <- rFunction(data = test_data, 
+                      timefilter = 5,
+                      bind_times = FALSE,
+                      createUTMs = FALSE,
+                      EPSG = 32733,
+                      bind_kmph = TRUE,
+                      speedcut = speed_thresh,
+                      bind_dist = FALSE,
+                      bind_timediff =FALSE,
+                      idcol = "",
+                      altitudecol = "",
+                      tempcol = "",
+                      headingcol = "",
+                      keepessentials = FALSE)
+  
+  expect_lt(max(actual$kmph, na.rm = TRUE), units::set_units(speed_thresh, "km/h"))
+  expect_lt(nrow(actual), nrow(test_data))
+})
+
+
+
 
 
 test_that("generated columns are binded", {
@@ -47,7 +77,7 @@ test_that("generated columns are binded", {
                   "secs",
                   "hourmin",
                   "yearmonthday",
-                  "gap_mins",
+                  "timediff_hrs",
                   "kmph",
                   "dist_m",
                   "geometry",
@@ -79,7 +109,6 @@ test_that("alternative EPSGs can still be handled", {
   expect(all(is.double(c(actual$x, actual$y))), failure_message = "Output UTMs are non-numeric")
   expect(dplyr::between(sf::st_coordinates(actual)[1,]["X"], 473631, 473632), failure_message = "UTM X-coordinate is incorrectly transformed")
   expect(dplyr::between(sf::st_coordinates(actual)[1,]["Y"], -5799990, -5799989), failure_message = "UTM Y-coordinate is incorrectly transformed")
-  
   
 })
 
@@ -120,5 +149,34 @@ test_that("misnamed columns throw error message", {
                                    tempcol = "",
                                    headingcol = "",
                                    keepessentials = FALSE))
+  
+})
+
+
+
+
+
+test_that("App throws error if input move2 object has no CRS specified", {
+  
+  dt <- test_data
+  sf::st_crs(dt) <- NA
+  
+  expect_error(
+    rFunction(data = dt), 
+    regexp = "App requires input data with a specified Coordinate Reference System."
+  )
+  
+})
+
+
+
+test_that("App throws error if specified ESPG code is non-valid", {
+
+  expect_error(
+    suppressWarnings( # really not good to suppress warnings in testing, but just trying to avoid having to built a withCallingHandlers() here. The warning message is a nuisance here
+      rFunction(data = test_data, EPSG = 155567717)
+    ), 
+    regexp = "Can't find the Coordinate Reference System for the provided `EPSG` code"
+  )
   
 })
